e16ac1972a2c8b3ec70e43999158ac8ce945b5fd
Vadim Glazkov <v.glazkov@corp.mail.ru>
2017-03-01 18:49:18 +0300

    output fixed. tmp element is list bug fixed

--- main.lisp
+++ main.lisp
@@ -6,74 +6,93 @@
   (not (eq x y))
 )
 
-(defun recursiveConcat (lst)
-  (cond ((null lst) "")
-	((concatenate 'string (car lst) " " (recursiveConcat (cdr lst))))
-  )
+(defun logIt (stringToLog) 
+  (and (print (concatenate 'string "LOG: " stringToLog)) T)
 )
 
-(defun logIt (&rest lst) 
-  (and (print (recursiveConcat (cons "LOG: " lst))) T)
+(defun logAndReturn (returnValue stringToLog)
+  (and (logIt stringToLog) returnValue)
 )
 
-(defun logAndReturn (&rest lst)
-  (and (logIt (cdr lst)) (car lst))
-)
-
-(defun fatalError (&rest lst)
-  (logIt 'FATAL 'ERROR lst)
+(defun fatalError (stringToLog)
+  (and (print (concatenate 'string "FATAL: " stringToLog)) T)
 )
 
 (defun refalVariable (lst)
-  (cond ((ne (length lst) 2) nil)
-        ((and (ne (car lst) 'e)
-              (ne (car lst) 's)
-              (ne (car lst) 'w)
-              (ne (car lst) 'v)
-         )
-            nil
-        )
-        (t)
+  (cond ((atom lst) nil)
+        ((ne (length lst) 2) nil)
+        ((or  (eq (car lst) 'e)
+              (eq (car lst) 's)
+              (eq (car lst) 'w)
+              (eq (car lst) 'v)
+        ))
+
   )
 )
+
+(defun sMatchRefalTemplate (tmp lst) 
+  (logIt "sMatchRefalTemplate")
+)
+
+
+(defun eMatchRefalTemplate (tmp lst) 
+  (logIt "eMatchRefalTemplate")
+)
+
+(defun vMatchRefalTemplate (tmp lst) 
+  (logIt "vMatchRefalTemplate")
+)
+
+
+(defun wMatchRefalTemplate (tmp lst) 
+  (logIt "wMatchRefalTemplate")
+)
+
+
 
 (defun matchRefalTemplate (tmp lst) 
   (cond
     ;Error program structure checking
     ((or (null tmp) (null lst) (atom (car tmp)) (ne (length (car tmp)) 2))
-     	(fatalError "matchRefalTemplate")
+         (fatalError "matchRefalTemplate")
     )
 
     ((eq (car (car tmp)) 's) (sMatchRefalTemplate tmp lst))
     ((eq (car (car tmp)) 'e) (eMatchRefalTemplate tmp lst))
     ((eq (car (car tmp)) 'v) (vMatchRefalTemplate tmp lst))
     ((eq (car (car tmp)) 'w) (wMatchRefalTemplate tmp lst))
+    ((fatalError "matchRefalTemplate"))
   )
 )
 (defun Match (tmp lst)
   (cond 
-    	;Template and list are empty. End of parsing
-    	((and (null tmp) (null lst)) (logAndReturn T 'Null 'Lists))
+        ;Template and list are empty. End of parsing
+        ((and (null tmp) (null lst)) T)
 
-	;Either template or list are empty. Mathing fails
+        ;Either template or list are empty. Mathing fails
         ((or (null tmp) (null lst)) nil)
 
-	;First template's element is atom and it is not equal to first list's element
+        ;First template's element is atom and it
+        ;is not equal to first list's element
         ((and (atom (car tmp)) (ne (car tmp) (car lst))) nil)
 
         ;Equal elements. Recursive continue
-	((and (atom (car tmp)) 
+        ((and (atom (car tmp)) 
               (eq (car tmp) (car lst))) 
                 (Match (cdr tmp) (cdr lst)))
 
-	;Refal variable in template
-	((refalVariable (car tmp)) (logIt 'RefalVar))
+        ;Refal variable in template
+        ((refalVariable (car tmp)) (matchRefalTemplate tmp lst))
 
-	;First template's and list's first elements are lists. Recursive continue
-	((and (Match (car tmp) (car lst)) (Match (cdr tmp) (cdr lst))))
+        ;First tmp's element is list
+        ((and (not (atom (car tmp))) (atom (car lst))) nil)
+        
+        ;First template's and list's first elements are lists.
+        ;Recursive continue
+        ((and (Match (car tmp) (car lst)) (Match (cdr tmp) (cdr lst))))
 
-	;Fatal error if no cond rule matched
-        ((fatalError 'matchFunction))
+        ;Fatal error if no cond rule matched
+        ((fatalError "matchFunction"))
   )
 )
 
